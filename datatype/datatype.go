ba0e49128f83c4acdbf8a8c2cb1dcd5974e24ec5
mahbubsprint <mahbubsprint310@gmail.com>
2025-11-04 21:05:44 +0600

    Float type exploring

--- datatype/datatype.go
+++ datatype/datatype.go
@@ -1,9 +1,15 @@
 package main
 
 import "fmt"
+import "github.com/shopspring/decimal"
 
 func main() {
 	var Y int16 = 32767
-	var Z int16 = 32768 // overflow
+	//var Z int16 = 32768 // overflow
+	var Z int16 = 3275 // overflow
     fmt.Println(Y+2, Y-2,  Z)
+
+	a:= float32(34.56)
+	fmt.Printf("The value of a is %f and type is %T\n", a, a)
+
 }
